072cdcfc6e34fb423c47ad87a89b760191561488
Samarth <115448290+SAMARTHAGARWAL77@users.noreply.github.com>
2025-09-18 20:58:22 +0530

    leetcode_EveryDay_Solution_2025-09-18

---
+++ 2025-09-18/leetcode_EveryDay_2025-09-18.cpp
@@ -0,0 +1,57 @@
+class TaskManager {
+    map<int,int>taskToUser; 
+    map<int,int>taskToPriority;
+    set<pair<int,int>> task;
+
+public:
+    TaskManager(vector<vector<int>>& tasks) {
+    for(auto i : tasks)
+    {   
+        int userId = i[0];
+        int taskId = i[1];
+        int priority = i[2];    
+    
+    task.insert({priority,taskId});
+    taskToUser[taskId] = userId;
+    taskToPriority[taskId] = priority; 
+    }    
+    
+    }
+    
+    void add(int userId, int taskId, int priority) {
+        task.insert({priority,taskId});
+        taskToUser[taskId] = userId;
+        taskToPriority[taskId] = priority;
+    }
+    
+    void edit(int taskId, int newPriority) {
+    int priority = taskToPriority[taskId];
+    task.erase({priority,taskId});
+    task.insert({newPriority,taskId});
+    taskToPriority[taskId] = newPriority;
+    }
+    
+    void rmv(int taskId) {
+    int Priority = taskToPriority[taskId];
+    task.erase({Priority, taskId});
+    taskToPriority.erase(taskId);
+    taskToUser.erase(taskId);   
+    }
+    
+    int execTop() {
+    if(task.size() == 0)return -1;
+    int taskId = task.rbegin() -> second;
+    int userId = taskToUser[taskId];
+    rmv(taskId);
+    return userId;
+    }
+};
+
+/**
+ * Your TaskManager object will be instantiated and called as such:
+ * TaskManager* obj = new TaskManager(tasks);
+ * obj->add(userId,taskId,priority);
+ * obj->edit(taskId,newPriority);
+ * obj->rmv(taskId);
+ * int param_4 = obj->execTop();
+ */
